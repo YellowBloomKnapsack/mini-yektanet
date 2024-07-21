121cc698bfbe0a4801cbd63e90dd388730127ee7
Kourosh Alinaghi <kouroshalinaghi@gmail.com>
2024-07-21 07:26:56 +0330

    Implement AdServer functionality

---
+++ AdServer/logic/logic.go
@@ -0,0 +1,101 @@
+package logic
+
+import (
+    "time"
+	"encoding/json"
+    "fmt"
+	"net/http"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+    "YellowBloomKnapsack/mini-yektanet/common/models"
+)
+
+const (
+    getAdsAPIPath = "localhsot:8082/ads" // TODO: make this an env var in panel
+)
+
+type AdDTO struct {
+    ID        uint
+    Text      string
+    ImagePath string
+    Bid       int64
+}
+
+func ToAdDTO(dbAd models.Ad) *AdDTO {
+    return &AdDTO{
+        ID:        dbAd.ID,
+        Text:      dbAd.Text,
+        ImagePath: dbAd.ImagePath,
+        Bid:       dbAd.Bid,
+    }
+}
+
+var adsList = make([]*AdDTO, 0)
+
+func isBetterThan(lhs, rhs *AdDTO) bool {
+    return rhs.Bid > lhs.Bid
+}
+
+func GetBestAd() (*AdDTO, error) {
+    if len(adsList) == 0 {
+        return nil, fmt.Errorf("no ad was found")
+    }
+
+    bestAd := adsList[0]
+
+    for _, ad := range adsList {
+        if isBetterThan(bestAd, ad) {
+            bestAd = ad
+        }
+    }
+
+    return bestAd, nil
+}
+
+// WARNING: not tested yet
+func updateAdsList() error {
+    fmt.Println("Yayyyyyyyyyy!")
+	resp, err := http.Get(getAdsAPIPath)
+	if err != nil {
+		return fmt.Errorf("failed to fetch ads: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to fetch ads: status code %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	var ads []models.Ad
+	if err := json.Unmarshal(body, &ads); err != nil {
+		return fmt.Errorf("failed to unmarshal ads: %v", err)
+	}
+
+	var newAdsList []*AdDTO
+	for _, ad := range ads {
+	    newAdsList = append(newAdsList, ToAdDTO(ad))
+	}
+
+	adsList = newAdsList
+
+	return nil
+}
+
+func StartTicker() {
+    interval, _ := strconv.Atoi(os.Getenv("ADS_FETCH_INTERVAL_SECS"))
+    ticker := time.NewTicker(time.Duration(interval) * time.Second)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ticker.C:
+            _ = updateAdsList()
+        }
+    }
+}
