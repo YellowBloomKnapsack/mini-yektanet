f26b89761052cde6db80e95787e51a4efec5bd63
Nima Rezaei <mail.nima.rezaei@gmail.com>
2024-07-22 00:41:34 -0700

    added websites and static files and js for basic testing

--- PublisherWebsite/main.go
+++ PublisherWebsite/main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"log"
 	"os"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+var currentSiteNames []string
 
 func main() {
 	if err := godotenv.Load(".env", "../common/.env"); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	r := gin.Default()
+	currentSiteNames = []string{
+		"varzesh3",
+		"zoomit",
+		"digikala",	
+		"sheypoor",
+		"filimo",
+	}
 
-	r.LoadHTMLGlob("templates/*")
+	r := gin.Default()
+	r.GET("/:siteName", getSite)
+	r.LoadHTMLGlob("html/*")
 	r.Static("/static", "./static")
 
 	port := os.Getenv("PUBLISHER_WEBSITE_PORT")
@@ -23,4 +34,17 @@ func main() {
 		port = "8084"
 	}
 	r.Run(":" + port)
+}
+
+func getSite(c *gin.Context) {
+	siteName := c.Param("siteName")
+	for _, name := range(currentSiteNames) {
+		if siteName == name {
+			c.HTML(http.StatusOK, siteName+".html", gin.H{
+				"title": siteName,
+			})
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": "not here"})
 }
