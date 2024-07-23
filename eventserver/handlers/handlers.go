1dcf363294395b1cd0cc1395bc593187edc92f00
aparsa <aparsa.khadem@gmail.com>
2024-07-23 12:15:52 +0330

    invikeClickEvent added

--- eventserver/handlers/handlers.go
+++ eventserver/handlers/handlers.go
@@ -22,6 +22,8 @@ type TokenRequest struct {
 	Token string `json:"token"`
 }
 
+//func invokeClickEvent()
+
 func PostClick(c *gin.Context) {
 	privateKey := os.Getenv("PRIVATE_KEY")
 	key, _ := base64.StdEncoding.DecodeString(privateKey)
@@ -39,38 +41,43 @@ func PostClick(c *gin.Context) {
 		return
 	}
 
+	c.JSON(http.StatusOK, gin.H{"link": data.RedirectPath})
+
 	_, present := clickTokens[token]
 	if !present {
 		clickTokens[token] = true
 
 		url := "http://" + os.Getenv("HOSTNAME") + ":" + os.Getenv("PANEL_PORT") + os.Getenv("INTERACTION_CLICK_API")
 
-		dataDto := dto.InteractionDto{
-			PublisherUsername: data.PublisherUsername,
-			ClickTime:         time.Now(),
-			AdID:              data.AdID,
-		}
-
-		jsonData, err := json.Marshal(dataDto)
-		if err != nil {
-			fmt.Printf("failed to marshal InteractionDto to JSON: %v\n", err)
-			return
-		}
-
-		resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-		if err != nil {
-			fmt.Printf("failed to send POST request: %v\n", err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusCreated {
-			fmt.Printf("failed to fetch ads: status code %d\n", resp.StatusCode)
-		}
+		go invokeClickEvent(data, url, time.Now())
 	}
 
 	// c.Redirect(http.StatusMovedPermanently, data.RedirectPath)
-	c.JSON(http.StatusOK, gin.H{"link":data.RedirectPath})
+}
+
+func invokeClickEvent(data *tokeninterface.CustomToken, url string, clickTime time.Time) {
+	dataDto := dto.InteractionDto{
+		PublisherUsername: data.PublisherUsername,
+		ClickTime:         clickTime,
+		AdID:              data.AdID,
+	}
+
+	jsonData, err := json.Marshal(dataDto)
+	if err != nil {
+		fmt.Printf("failed to marshal InteractionDto to JSON: %v\n", err)
+		return
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Printf("failed to send POST request: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		fmt.Printf("failed to fetch ads: status code %d\n", resp.StatusCode)
+	}
 }
 
 func PostImpression(c *gin.Context) {
