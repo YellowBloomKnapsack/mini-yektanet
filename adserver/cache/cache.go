07df99f2c496c421b6c47652f7c0cdc0614ad11f
Kourosh Alinaghi <kouroshalinaghi@gmail.com>
2024-07-28 03:21:52 +0330

    Update adservercache and main

--- adserver/cache/cache.go
+++ adserver/cache/cache.go
@@ -2,17 +2,18 @@ package cache
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"time"
-	"strconv"
 	"os"
+	"strconv"
+	"time"
 
 	"YellowBloomKnapsack/mini-yektanet/common/cache"
 	"github.com/redis/go-redis/v9"
 )
 
 type AdServerCache struct {
-	redisClient *redis.Client
+	redisClient   *redis.Client
 	brakeDuration time.Duration
 }
 
@@ -20,22 +21,24 @@ func NewAdServerCache(redisUrl string) cache.CacheInterface {
 	brakeSeconds, _ := strconv.Atoi(os.Getenv("BRAKE_DURATION_SECS"))
 	return &AdServerCache{
 		redisClient: redis.NewClient(&redis.Options{
-			Addr:	  redisUrl,
-        	Password: "", // no password set
-        	DB:		  0,  // use default DB
+			Addr:     redisUrl,
+			Password: "", // no password set
+			DB:       0,  // use default DB
 		}),
 		brakeDuration: time.Duration(brakeSeconds) * time.Second,
 	}
 }
 
-func (r *AdServerCache) IsPresent(adID string) bool {
+func (r *AdServerCache) IsPresent(token string) bool {
+	fmt.Println(token)
 	ctx := context.Background()
-	_, err := r.redisClient.Get(ctx, adID).Result()
+	present, err := r.redisClient.Exists(ctx, token).Result()
 	if err != nil {
-		log.Println("could not search for " + adID + " on Adserver redis: " + err.Error())
+		log.Print(err)
+		return false
 	}
-	// note: in the case of actual error in connection, we still return false - i.e. ad not braked
-	return (err == redis.Nil)
+
+	return bool(present == 1)
 }
 
 func (r *AdServerCache) Add(adID string) {
