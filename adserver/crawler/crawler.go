1cb0b3293085abf2464a40d8bd034f5500b84104
Kourosh Alinaghi <kouroshalinaghi@gmail.com>
2024-07-31 14:04:16 +0330

    Update crawler

--- adserver/crawler/crawler.go
+++ adserver/crawler/crawler.go
@@ -1,15 +1,19 @@
-package main
+package crawler
 
 import (
 	"YellowBloomKnapsack/mini-yektanet/adserver/kvstorage"
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"golang.org/x/net/html"
 )
@@ -18,6 +22,7 @@ type Crawler struct {
 	kvstorage        kvstorage.KVStorageInterface
 	persianStopWords map[string]bool
 	numOftopwords    int
+	urls             map[string]int
 }
 
 // Publisher IDs.
@@ -30,6 +35,7 @@ var publisherIDs = map[string]int{
 }
 
 func NewCrawler(kvstorage kvstorage.KVStorageInterface) *Crawler {
+	baseUrl := "http://" + os.Getenv("PUBLISHER_WEBSITE_HOSTNAME") + ":" + os.Getenv("PUBLISHER_WEBSITE_PORT")
 	return &Crawler{
 		kvstorage: kvstorage,
 		persianStopWords: map[string]bool{
@@ -42,19 +48,30 @@ func NewCrawler(kvstorage kvstorage.KVStorageInterface) *Crawler {
 			"کنید": true, "بدانید": true, "خوش": true, "آمدید": true, "خود": true, "زیاد": true, "کم": true, "زیادی": true,
 		},
 		numOftopwords: 5,
+		urls: map[string]int{
+			baseUrl + "/varzesh3": 1,
+			baseUrl + "/digikala": 2,
+			baseUrl + "/zoomit":   3,
+			baseUrl + "/sheypoor": 4,
+			baseUrl + "/filimo":   5,
+		},
 	}
 }
 
-func (c *Crawler) readHTML(filePath string) ([]byte, error) {
-	file, err := os.Open(filePath)
+func (c *Crawler) readHTML(url string) ([]byte, error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("error opening file: %v", err)
+		return nil, fmt.Errorf("error fetching URL: %v", err)
 	}
-	defer file.Close()
+	defer resp.Body.Close()
 
-	content, err := ioutil.ReadAll(file)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error fetching URL: received status code %d", resp.StatusCode)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("error reading file: %v", err)
+		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 
 	return content, nil
@@ -124,17 +141,25 @@ func (c *Crawler) findTopWords(text string) []string {
 }
 
 func (c *Crawler) Crawl() {
-	filePaths := map[string]int{
-		"/home/aparsa/Desktop/yek/mini-yektanet/publisherwebsite/html/varzesh3.html": 1,
-		"/home/aparsa/Desktop/yek/mini-yektanet/publisherwebsite/html/digikala.html": 2,
-		"/home/aparsa/Desktop/yek/mini-yektanet/publisherwebsite/html/zoomit.html":   3,
-	}
+	c.crawlOnce()
 
-	resultChan := make(chan string)
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
 
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				c.crawlOnce()
+			}
+		}
+	}()
+}
+
+func (c *Crawler) crawlOnce() {
 	var wg sync.WaitGroup
 
-	for filePath, publisherID := range filePaths {
+	for filePath, publisherID := range c.urls {
 		wg.Add(1)
 
 		go func(filePath string, publisherID int) {
@@ -142,32 +167,26 @@ func (c *Crawler) Crawl() {
 
 			content, err := c.readHTML(filePath)
 			if err != nil {
-				resultChan <- fmt.Sprintf("%d+%s: Failed to read file: %v", publisherID, filePath, err)
-				return
+				log.Printf("%d+%s: Failed to read file: %v", publisherID, filePath, err)
 			}
 
 			node, err := html.Parse(bytes.NewReader(content))
 			if err != nil {
-				resultChan <- fmt.Sprintf("%d+%s: Failed to parse HTML: %v", publisherID, filePath, err)
-				return
+				log.Printf("%d+%s: Failed to parse HTML: %v", publisherID, filePath, err)
 			}
 
 			rawText := c.extractText(node)
 			normalizedText := c.normalizeText(rawText)
 
 			topWords := c.findTopWords(normalizedText)
+			publisherIDStr := strconv.Itoa(int(publisherID))
 
-			result := fmt.Sprintf("%d+%s: %s", publisherID, filePath, strings.Join(topWords, ", "))
-			resultChan <- result
+			c.kvstorage.Set(publisherIDStr, strings.Join(topWords, ","))
+			log.Printf("%d+%s: %s", publisherID, filePath, strings.Join(topWords, ", "))
 		}(filePath, publisherID)
 	}
 
 	go func() {
 		wg.Wait()
-		close(resultChan)
 	}()
-
-	for result := range resultChan {
-		fmt.Println(result)
-	}
 }
